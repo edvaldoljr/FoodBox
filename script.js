d593d6b5d3455bca825c9d1359e6492dd67df94d
edvaldoljr <edvaldoluizj@gmail.com>
2021-09-12 00:59:30 -0300

    Organizacao de arquivos

---
+++ script.js
@@ -0,0 +1,26 @@
+function escolherFrango() {
+    document.getElementById("Frango").style.borderColor = "green";
+    document.getElementById("Carne").style.borderColor = "white";
+  }
+  function escolherCarne() {
+    document.getElementById("Carne").style.borderColor = "green";
+    document.getElementById("Frango").style.borderColor = "white";
+  }
+
+  function escolherCoca() {
+    document.getElementById("Coca").style.borderColor = "green";
+    document.getElementById("Suco").style.borderColor = "white";
+  }
+  function escolherSuco() {
+    document.getElementById("Suco").style.borderColor = "green";
+    document.getElementById("Coca").style.borderColor = "white";
+  }
+
+  function escolherPudim() {
+    document.getElementById("Pudim").style.borderColor = "green";
+    document.getElementById("Sorvete").style.borderColor = "white";
+  }
+  function escolherSorvete() {
+    document.getElementById("Sorvete").style.borderColor = "green";
+    document.getElementById("Pudim").style.borderColor = "white";
+  }
