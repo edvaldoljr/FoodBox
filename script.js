cd690979abad78cdcfd4f228d6e2678c73bd8dc3
Edvaldo Luiz da Silva Junior <77884468+edvaldoljr@users.noreply.github.com>
2021-09-17 01:05:48 -0300

    Pegaritens escolhedios

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 let prato;
+let bebida;
+let sobremesa;
 
 function escolherFrango() {
   document.getElementById("Frango").style.borderColor = "green";
   document.getElementById("Carne").style.borderColor = "white";
-  prato = "Frango";
+  prato = "Frango Yin-Yang";
 }
 function escolherCarne() {
   document.getElementById("Carne").style.borderColor = "green";
   document.getElementById("Frango").style.borderColor = "white";
+  prato = "Carne e Salada";
 }
 
 function escolherCoca() {
   document.getElementById("Coca").style.borderColor = "green";
   document.getElementById("Suco").style.borderColor = "white";
+  bebida = "Coca-Cola";
 }
 function escolherSuco() {
   document.getElementById("Suco").style.borderColor = "green";
   document.getElementById("Coca").style.borderColor = "white";
+  bebida = "Suco de Laranja";
 }
 
 function escolherPudim() {
   document.getElementById("Pudim").style.borderColor = "green";
   document.getElementById("Sorvete").style.borderColor = "white";
+  sobremesa = "Pudim";
 }
 function escolherSorvete() {
   document.getElementById("Sorvete").style.borderColor = "green";
   document.getElementById("Pudim").style.borderColor = "white";
+  sobremesa = "Sorvete de Morango";
 }
 function finalizarPedido() {
-  alert(prato);
+  alert(prato + ", " + bebida + ", " + sobremesa);
 }
